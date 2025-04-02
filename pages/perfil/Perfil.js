ba0dc77d896640a9b62137c9fd731ed043bdfdb0
Amanda Gomes Mechi <amandamechi327@gmail.com>
2025-04-01 22:36:27 -0300

    finished

--- pages/perfil/Perfil.js
+++ pages/perfil/Perfil.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-const DetalhesScreen = ({ route, navigation }) => {
-    const { textoDigitado = 'Nenhum texto disponível', textoSalvo = 'Nenhum texto armazenado' } = route.params;
-
+export default function PerfilScreen() {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Detalhes do Texto</Text>
-            <Text style={styles.normalText}>Texto Atual: {textoDigitado}</Text>
-            <Text style={styles.savedText}>Texto Salvo: {textoSalvo}</Text>
-            <View style={styles.buttonContainer}>
-                <Button title='Voltar' onPress={() => navigation.goBack()} color='blue' />
+            <Text style={styles.title}>Amanda</Text>
+            <Text style={styles.description}>
+               tds1
+            </Text>
+            <Image source={{ uri: 'https://avatars.githubusercontent.com/u/158229094?v=4' }} style={styles.profileImage} />
+            <View style={styles.iconContainer}>
+                <Ionicons name='logo-html5' size={40} color='pink' />
+                <Ionicons name='logo-css3' size={40} color='blue' />
+                <Ionicons name='logo-javascript' size={40} color='yellow' />
+                <Ionicons name='logo-react' size={40} color='cyan' />
             </View>
         </View>
     );
-};
-const styles = StyleSheet.create({
-    container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#EAF6F6' },
-    title: { fontSize: 22, fontWeight: 'bold', marginBottom: 15 },
-    normalText: { color: '#FF5733', marginBottom: 10 },
-    savedText: { color: '#33AFFF', marginBottom: 10 },
-    buttonContainer: { width: '80%', marginTop: 10 }
-});
+}
 
-export default DetalhesScreen;
+const styles = StyleSheet.create({
+    container: { flex: 1, alignItems: 'center', justifyContent: 'center', padding: 20, backgroundColor: '#EAF6F6' },
+    title: { fontSize: 22, fontWeight: 'bold', marginBottom: 10 },
+    description: { fontSize: 14, textAlign: 'center', marginBottom: 10 },
+    profileImage: { width: 250, height: 150, borderRadius: 10, marginBottom: 20 },
+    iconContainer: { flexDirection: 'row', justifyContent: 'space-around', width: '80%', marginTop: 20 }
+});
